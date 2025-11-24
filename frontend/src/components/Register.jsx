eae24764d17c1fe496b7b98f10af402235553d5f
Badhan Sarkar <badhansarkar164@gmail.com>
2025-11-24 12:02:10 +0530

    work on registration

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { useState } from "react"
+import axios from "axios"
+
 
 const Register = () => {
+
+  const [username , setUsername] = useState('')
+  const [email , setEmail] = useState('')
+  const [password , setPassword] = useState('')
+  const handlelRegistration = async (e) => {
+    e.preventDefault()
+
+    const userData = {
+      username, email , password
+    }
+
+    try{
+      const response = await axios.post('http://127.0.0.1:8000/api/v1/register/',userData)
+      console.loh('Response data was',response.data)
+      console.loh('Registrarion Successfull')
+    }catch{
+      console.error('Registration Failed:',error.response.data)
+    }
+
+  }
+
   return (
-    <div>
-      <h1 className='text-white'>fv</h1>
+    <>
+    <div className='container'>
+        <div className="row justify-content-center">
+            <div className="col-md-6 bg-light-dark p-5 rounded">
+                <h3 className='text-light text-center mb-5'>Create an Account</h3>
+                <form onSubmit={handlelRegistration}>
+                  <input type="text" className="form-control mb-3"  placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                  <input type="email" className="form-control mb-3"  placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                  <input type="password" className="form-control mb-3"  placeholder="Set password" value={password} onChange={(e) => setPassword(e.target.value) }/>
+                  <button type="submit" className="btn btn-info d-block mx-auto">Register</button>
+                </form>
+            </div>
+        </div>
     </div>
+    </>
   )
 }
 
 export default Register
+
